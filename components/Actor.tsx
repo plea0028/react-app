0cfdecbad4252ec5357a59f5cfebf5b816a8a978
James Pleau <plea0028@algonquinlive.com>
2025-03-27 18:41:00 -0400

    finishing card

--- components/Actor.tsx
+++ components/Actor.tsx
@@ -1,12 +1,20 @@
 import {Result} from '../app/types'
+import styles from './Actor.module.css'
 
 export default function Actor({actor}: { actor : Result}) {
+
+  if (!actor) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div>
-      Name: {actor.name}
-      Gender: {actor.gender}
-      <p>Known for</p>
-      <ul>
+    <div className={styles.actorCard}>
+      <img src={actor.profile_path ? `https://image.tmdb.org/t/p/w500${actor.profile_path}` : "/images/placeholder.jpg"}
+      alt={actor.name}
+      className={styles.image} />
+      <h1 className={styles.title}>{actor.name}</h1>
+      <h2 className={styles.subtitle}>Known for</h2>
+      <ul className={styles.knownForList}>
         {actor.known_for.map((movie, index) => <li key={index}>{movie.title}</li>)}
       </ul>
     </div>
